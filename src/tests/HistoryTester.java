478d439510a2e72a0e19224567e18823794b5745
whitegoogles <daviesj@msoe.edu>
2016-01-04 13:17:34 -0600

    Added a tests package with all of the stub methods for testing the history object (like clearing it, getting a result from it, and displaying it - Related to user stories 5,6, and 7.

---
+++ src/tests/HistoryTester.java
@@ -0,0 +1,42 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HistoryTester {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testShowHistory() {
+        
+    }
+    public void testEmptyHistory(){
+        
+    }
+    public void testMaxHistory(){
+        
+    }
+    public void testClearHistory(){
+        
+    }
+    public void testClearHistoryEmpty(){
+        
+    }
+    public void testGetHistory(){
+        
+    }
+    public void testGetHistoryBadIndex(){
+        
+    }
+}
