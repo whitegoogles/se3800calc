f5b7b519cb14df5bdc3a4b9eca9368666df2ac1a
whitegoogles <daviesj@msoe.edu>
2016-01-04 17:07:50 -0600

    Filled out several tests for the history object - Related to user stories 5,6, and 7.

--- src/tests/HistoryTester.java
+++ src/tests/HistoryTester.java
@@ -3,14 +3,17 @@ package tests;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import se3800.History;
+import se3800.HistoryInterface;
+
 public class HistoryTester {
+    HistoryInterface myHistory;
     @Before
     public void setUp() throws Exception {
+        myHistory = new History();
     }
 
     @After
@@ -18,34 +21,46 @@ public class HistoryTester {
     }
     @Test
     public void testAddHistory(){
-        
-    }
-    @Test
-    public void testShowHistory() {
-        
-    }
-    @Test
-    public void testEmptyHistory(){
-        
+        String [] results = {"mas","aig rean","ad audi","First","Chain",
+                             "1","2","12","x","aksdfj","kasdjf"}; 
+        for(String result: results){
+            myHistory.addResult(result);
+        }
+        for(int i = 0; i<results.length; i++){
+            assertEquals(results[i],myHistory.getResult(i));
+        }
     }
     @Test
     public void testMaxHistory(){
-        
+        for(int i = 0; i<=1001; i++){
+            myHistory.addResult(""+i);
+        }
+        for(int i = 0; i<1000; i++){
+            assertEquals(i+1,myHistory.getResult(i));
+        }
     }
     @Test
-    public void testClearHistory(){
+    public void testEmptyHistory(){
+        String [] addMe = {"a","b","d","e"};
+        for(String a: addMe){
+            myHistory.addResult(a);
+        }
+        myHistory.clear();
         
+        //This should throw an exception or something
+        myHistory.getResult(0); 
+        fail(); 
     }
+    
     @Test
     public void testClearHistoryEmpty(){
         
     }
+    
     @Test
-    public void testGetHistory(){
-        
-    }
-    @Test
-    public void testGetHistoryBadIndex(){
-        
+    public void testGetHistoryBadIndex(){        
+        //This should throw an exception or something
+        myHistory.getResult(0); 
+        fail(); 
     }
 }
